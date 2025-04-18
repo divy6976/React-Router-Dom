857419510e49e2d221ac02c44b1aa5f0e61bbd27
divy6976 <divyprakashpandey6@gmail.com>
2025-04-18 17:11:34 +0530

    first commit

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect,useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data=useLoaderData()
+    // const [data,setData]=useState([])
+    // useEffect(() => {
+      
+    //     fetch('https://api.github.com/users/divy6976')
+    //     .then(res=>res.json())
+    //     .then(data =>{
+    //         setData(data)
+    //     })
+    
+    // }, [])
+    
+  return (
+    <div className="text-center m-5 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img  src={data.avatar_url} alt="Git-picture" width={200} />
+    </div>
+  )
+}
+
+export default Github
+
+
+export const GithubInfoLoader =async ()=>{
+
+
+    const response = await fetch('https://api.github.com/users/divy6976')
+
+    return response.json()
+}
